fe32749b479557fdc1e1f7dd751c530244f3c4ce
wichru <wicher.maciej@gmail.com>
2018-08-28 00:00:04 +0200

    create artiles action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
   def index
 
   end
+
+  def new
+  end
+
+  def create
+    post_params = params.require(:post).permit(:title, :text)
+
+  end
+
+  def edit; end
+
+  def show; end
+
 end
